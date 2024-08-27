470535bf862881b90a99127099bd22870b2fd36d
Brunwo <bwagner57@gmail.com>
2024-08-27 18:00:35 +0200

    Auto-commit: mar. 27 août 2024 18:00:35 CEST

--- zip_source.sh
+++ zip_source.sh
@@ -1,37 +1,44 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Function to print usage
 print_usage() {
-    echo "Usage: zip_source <source_directory> [<output_zip_file>]"
+    echo "Usage: zip_source <source_directory> [<output_directory>]"
     echo "Zips the contents of a directory, excluding files specified in .gitignore"
+    echo "If no output directory is provided, it creates the zip file in the source directory"
 }
 
-# Check if the directory is provided
+# Check if the source directory is provided
 if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     print_usage
     exit 1
 fi
 
 SOURCE_DIR=$1
-OUTPUT_FILE=${2:-source_code.zip}
+# Get the basename of the source directory for the zip name
+ZIP_NAME=$(basename "$(realpath "$SOURCE_DIR")").zip
 
-# Get the absolute path of the source directory
+# Set output directory
+OUTPUT_DIR=${2:-"$SOURCE_DIR"}
+
+# Get the absolute paths
 ABSOLUTE_SOURCE_DIR=$(realpath "$SOURCE_DIR")
+ABSOLUTE_OUTPUT_DIR=$(realpath "$OUTPUT_DIR")
 
 # Create a temporary directory to hold the files to be zipped
 TEMP_DIR=$(mktemp -d)
 
 # Sync the files to the temporary directory, excluding those in .gitignore
-rsync -av --exclude-from="$ABSOLUTE_SOURCE_DIR/.gitignore" --exclude=".git" "$ABSOLUTE_SOURCE_DIR/" "$TEMP_DIR/"
+rsync -a --exclude-from="$ABSOLUTE_SOURCE_DIR/.gitignore" --exclude=".git" "$ABSOLUTE_SOURCE_DIR/" "$TEMP_DIR/"
 
 # Zip the contents of the temporary directory
-cd "$TEMP_DIR" || { echo "Failed to change directory to $TEMP_DIR"; exit 1; }
-zip -r "$OUTPUT_FILE" . -q
+(cd "$TEMP_DIR" && zip -r "$ZIP_NAME" . -q) || { echo "Failed to create zip file"; exit 1; }
 
-# Move the zip file to the original directory
-mv "$OUTPUT_FILE" "$ABSOLUTE_SOURCE_DIR/"
+# Move the zip file to the output directory
+mv "$TEMP_DIR/$ZIP_NAME" "$ABSOLUTE_OUTPUT_DIR/" || { echo "Failed to move zip file to output directory"; exit 1; }
 
 # Clean up the temporary directory
 rm -rf "$TEMP_DIR"
 
-echo "Source code from '$SOURCE_DIR' has been zipped into '$SOURCE_DIR/$OUTPUT_FILE', excluding files in .gitignore."
+echo "Source code from '$SOURCE_DIR' has been zipped into '$ABSOLUTE_OUTPUT_DIR/$ZIP_NAME', excluding files in .gitignore."
